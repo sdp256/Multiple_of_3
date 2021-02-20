612381f458578bb5a7c142892d6353a03bd9bd56
sdp256 <steevnpairy@gmail.com>
2021-02-19 18:01:43 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Multiple_of_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count;
+
+            for (count = 1; count <= number; count++)
+            {
+                if (count % 3 == 0)
+                {
+                    Console.WriteLine("*");
+                }
+                else
+                {
+                    Console.WriteLine(count);
+                }   
+            }
+         
+            
+
+
+
+
+
+           // number % 3 = 0 finding multiple of 3?
+        }
+    }
+}
